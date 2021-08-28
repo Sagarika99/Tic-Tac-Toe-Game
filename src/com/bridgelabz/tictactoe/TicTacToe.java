77cf5fab8e88c904ea91e3bf45868a7a7262fcd8
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-28 11:36:32 +0530

    Created TicTacToe board

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -2,7 +2,15 @@ package com.bridgelabz.tictactoe;
 
 public class TicTacToe {
 
+	public void board() {
+		char[] board = new char[10];
+		for (int i=0;i<10;i++) {
+			board[i] = ' ';
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		
-	}
+		}
 }
