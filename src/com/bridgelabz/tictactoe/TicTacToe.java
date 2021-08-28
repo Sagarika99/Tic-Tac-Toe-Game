c21a3b861e9d27ab773721bebcaac263245ab665
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-28 11:58:06 +0530

    Player Choice and Computer Choice taken

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,16 +1,33 @@
 package com.bridgelabz.tictactoe;
+import java.util.Scanner;
 
 public class TicTacToe {
 
-	public void board() {
+	public char playerChoice;
+	public char computerChoice;
+	
+	public  void board() {
 		char[] board = new char[10];
 		for (int i=0;i<10;i++) {
 			board[i] = ' ';
 		}
 	}
 	
-	
-	public static void main(String[] args) {
+	public void playerInput() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Player choice: X or O");
+		playerChoice = sc.next().charAt(0);
 		
+		if (playerChoice == 'X') {
+			computerChoice = 'O';
 		}
+		else {
+			computerChoice = 'X';
+		}
+		System.out.println("Player Choice: "+playerChoice);
+		System.out.println("Computer Choice: "+computerChoice);
+		
+		
+	}
+	
 }
